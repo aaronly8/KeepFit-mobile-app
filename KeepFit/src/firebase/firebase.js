7f7c1ff3831e5c1bec30b498dae219d7d2f3363a
Sajan Gutta <gutta@usc.edu>
2021-03-07 23:12:57 -0800

    adding firebase configurations

---
+++ KeepFit/src/firebase/firebase.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import firebaseConfig from '../firebase/config.js';
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export default db;
