c5af3ad7281a3756f3ec4f1e638495c64580cbd8
Sajan Gutta <gutta@usc.edu>
2021-03-08 19:55:43 -0800

    logging into firebase once google sign in is complete

--- KeepFit/src/navigation/RootStackNavigator.js
+++ KeepFit/src/navigation/RootStackNavigator.js
@@ -15,6 +15,7 @@ import MainScreen from '@app/screens/main';
 
 import Color from '@app/theme/color'
 import { log } from 'react-native-reanimated'
+import { db, firebase } from "../firebase/firebase.js"
 
 const Stack = createStackNavigator()
 
@@ -44,15 +45,96 @@ function RootStackNavigator() {
         initialScreen = "Main"
     }
 
-    async function signInWithGoogleAsync() {
+    const isUserEqual = (googleUser, firebaseUser) => {
+        if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+                if (
+                    providerData[i].providerId ===
+                    firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                    providerData[i].uid === googleUser.getBasicProfile().getId()
+                ) {
+                    // We don't need to reauth the Firebase connection.
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    const onSignIn = googleUser => {
+        console.log("called on signin");
+        console.log('Google Auth Response', googleUser);
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        var unsubscribe = firebase.auth().onAuthStateChanged(
+            function (firebaseUser) {
+                unsubscribe();
+                // Check if we are already signed-in Firebase with the correct user.
+                if (!isUserEqual(googleUser, firebaseUser)) {
+                    // Build Firebase credential with the Google ID token.
+                    var credential = firebase.auth.GoogleAuthProvider.credential(
+                        googleUser.idToken,
+                        googleUser.accessToken
+                    );
+                    console.log("built credential");
+                    // Sign in with credential from the Google user.
+                    firebase
+                        .auth()
+                        .signInAndRetrieveDataWithCredential(credential)
+                        .then(function (result) {
+                            console.log('user signed in ');
+                            if (result.additionalUserInfo.isNewUser) {
+                                console.log("trying database create");
+                                firebase
+                                    .database()
+                                    .ref('/users/' + result.user.uid)
+                                    .set({
+                                        gmail: result.user.email,
+                                        profile_picture: result.additionalUserInfo.profile.picture,
+                                        first_name: result.additionalUserInfo.profile.given_name,
+                                        last_name: result.additionalUserInfo.profile.family_name,
+                                        created_at: Date.now()
+                                    })
+                                    .then(function (snapshot) {
+                                        // console.log('Snapshot', snapshot);
+                                    });
+                            } else {
+                                firebase
+                                    .database()
+                                    .ref('/users/' + result.user.uid)
+                                    .update({
+                                        last_logged_in: Date.now()
+                                    });
+                            }
+                        })
+                        .catch(function (error) {
+                            // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            // The email of the user's account used.
+                            var email = error.email;
+                            // The firebase.auth.AuthCredential type that was used.
+                            var credential = error.credential;
+                            // ...
+                        });
+                } else {
+                    console.log('User already signed-in Firebase.');
+                }
+            }.bind(this)
+        );
+    };
+
+    const signInWithGoogleAsync = async () => {
         try {
             const result = await Google.logInAsync({
                 androidClientId: googleandroidClientId,
                 iosClientId: googleiosClientId,
-                scopes: ['profile', 'email'],
+                scopes: ['profile', 'email']
             });
 
             if (result.type === 'success') {
+                console.log("calling onsignin function");
+                onSignIn(result);
                 return result.accessToken;
             } else {
                 return { cancelled: true };
@@ -60,7 +142,7 @@ function RootStackNavigator() {
         } catch (e) {
             return { error: true };
         }
-    }
+    };
 
     return (
         <NavigationContainer
