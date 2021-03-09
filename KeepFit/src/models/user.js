39933666edc2a486e2ea7391951ca93e47b74607
Sajan Gutta <gutta@usc.edu>
2021-03-08 19:04:08 -0800

    small update to user model

--- KeepFit/src/models/user.js
+++ KeepFit/src/models/user.js
@@ -1,14 +1,16 @@
 import BaseModel from "../models/base";
 
 class User extends BaseModel {
-    constructor (id, full_name, email, username, gender, weight, height, fitness_level, created_at) {
+    constructor (id, full_name, email, photo_url, username, birthday, gender, weight, height, fitness_level, created_at) {
         // initialize base model with firebase collection name
         super("users");
         
         self.id = id
         self.full_name = full_name
         self.email = email
+        self.photo_url = photo_url
         self.username = username
+        self.birthday = birthday
         self.gender = gender
         self.weight = weight
         self.height = height
