9eae8a458e6a39243389cd7053d08a3b491d1476
Sajan Gutta <gutta@usc.edu>
2021-03-13 15:31:21 -0800

    setting up search exercises screen

---
+++ KeepFit/src/screens/explore/exercises.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Button } from 'react-native';
+import Container from '@app/components/container.js'
+import { Header } from '@app/components/text.js';
+
+const SearchExercisesScreen = props => {
+    return (
+        <SafeAreaView>
+            <Container>
+                <Button title="<< Back" onPress={() => props.changeScreenHandler("index")} />
+                <Header style={styles.mainHeader}>
+                    Welcome to the Search Exercises Screen!
+                </Header>
+            </Container>
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    mainHeader: {
+        fontSize: 50,
+        marginTop: "55%",
+        textAlign: "center"
+    }
+});
+
+export default SearchExercisesScreen;
--- KeepFit/src/screens/explore/index.js
+++ KeepFit/src/screens/explore/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Button } from 'react-native';
 import Container from '@app/components/container.js'
 import { Header } from '@app/components/text.js';
+import SearchExercisesScreen from '../explore/exercises';
 
 const ExploreScreen = props => {
+    const [displayedScreen, setDisplayedScreen] = useState('index');
+
+    const changeScreenHandler = (new_screen) => {
+        setDisplayedScreen(new_screen);
+    };
+
+    let visibleContent;
+    if (displayedScreen == "exercises") {
+        visibleContent = <SearchExercisesScreen changeScreenHandler={changeScreenHandler} />
+    }
+
     return (
         <SafeAreaView>
-            <Container>
-                <Header style={styles.mainHeader}>
-                    Welcome to the Explore Screen!
-                </Header>
-            </Container>
+                {!visibleContent ? (
+                    <Container>
+                        <Header style={styles.mainHeader}>
+                            Welcome to the Explore Screen!
+                        </Header>
+                        <Button onPress={() => changeScreenHandler("exercises")} title="Search Exercises" />
+                    </Container>
+                ) : (
+                    visibleContent)}
         </SafeAreaView>
     )
 };
