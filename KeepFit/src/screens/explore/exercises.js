27107ac9df0dcee128e45d8277f0617b20a45f4d
Sajan Gutta <gutta@usc.edu>
2021-03-15 00:30:38 -0700

    update to message on home page, adding details initial logic for exercise search

--- KeepFit/src/screens/explore/exercises.js
+++ KeepFit/src/screens/explore/exercises.js
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Button, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, Button, Text, TouchableHighlight, View } from 'react-native';
 import Container from '@app/components/container.js'
 import { Header } from '@app/components/text.js';
 import db from "../../firebase/firebase";
 import Exercise from "../../models/exercise"
+
+const DetailsScreen = props => {
+    return (
+        <SafeAreaView>
+            <Button title="<< Back" onPress={() => props.detailsBackHandler()} />
+            <Header>
+                {props.exerciseName}
+            </Header>
+        </SafeAreaView>
+    );
+};
 
 const SearchExercisesScreen = props => {
     const [filteredExerciseDictionary, setFilteredExerciseDictionary] = useState({});
     const [muscleGroupFilter1, setMuscleGroupFilter1] = useState("");
     const [muscleGroupFilter2, setMuscleGroupFilter2] = useState("");
     const [categoryFilter, setCategoryFilter] = useState("");
+    const [displayedDetails, setDisplayedDetails] = useState(null);
 
     useEffect(() => {
         var exerciseDictionary = {};
@@ -21,25 +33,52 @@ const SearchExercisesScreen = props => {
             console.log("just ran");
         });
     }, []);
-    
+
+    const detailsBackHandler = () => {
+        setDisplayedDetails(null);
+    };
 
     return (
-        <SafeAreaView>
-            <Container>
-                <Button title="<< Back" onPress={() => props.changeScreenHandler("index")} />
-                {Object.entries(filteredExerciseDictionary).map(exercise =>
-                    <Text>{exercise[1].name}</Text>
+        <SafeAreaView style={styles.searchContainer}>
+                {displayedDetails ? (
+                    <DetailsScreen 
+                        exerciseName={filteredExerciseDictionary[displayedDetails].name}
+                        detailsBackHandler={detailsBackHandler}
+                    />
+                ) : (
+                <View style={styles.listView}>
+                    <Button title="<< Back" onPress={() => props.changeScreenHandler("index")} />
+                    <ScrollView>
+                    {Object.entries(filteredExerciseDictionary).map(exercise =>
+                        <TouchableHighlight
+                        onPress={() => {
+                            setDisplayedDetails(exercise[0]);
+                        }}>
+                            <Text style={styles.exerciseText}>{exercise[1].name}</Text>
+                        </TouchableHighlight>
+                    )}
+                    </ScrollView>
+                </View>
                 )}
-            </Container>
         </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
+    searchContainer: {
+        paddingHorizontal: 25
+    },
     mainHeader: {
         fontSize: 50,
         marginTop: "55%",
         textAlign: "center"
+    },
+    exerciseText: {
+        fontSize: 30,
+        marginTop: 10,
+    },
+    listView: {
+        marginBottom: 200
     }
 });
 
--- KeepFit/src/screens/home/index.js
+++ KeepFit/src/screens/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Container from '@app/components/container.js'
-import { Header } from '@app/components/text.js';
+import { Header, Subheader } from '@app/components/text.js';
 
 const HomeScreen = props => {
     return (
@@ -10,6 +10,9 @@ const HomeScreen = props => {
                 <Header style={styles.mainHeader}>
                     Welcome to the Home Screen!
                 </Header>
+                <Subheader style={styles.subHeader}>
+                    A Feed is Coming Here in a Future Sprint!
+                </Subheader>
             </Container>
         </SafeAreaView>
     )
@@ -19,6 +22,11 @@ const styles = StyleSheet.create({
     mainHeader: {
         fontSize: 50,
         marginTop: "55%",
+        textAlign: "center"
+    },
+    subHeader: {
+        fontSize: 18,
+        marginTop: 0,
         textAlign: "center"
     }
 });
