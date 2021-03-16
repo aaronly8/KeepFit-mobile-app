c80f9e2f69a9065ef839a0a2d4fa9dab991c3fb3
Sajan Gutta <gutta@usc.edu>
2021-03-16 01:08:13 -0700

    profile screen cleanup

--- KeepFit/src/screens/profile/index.js
+++ KeepFit/src/screens/profile/index.js
@@ -1,13 +1,12 @@
-/*import React from 'react';
-import { SafeAreaView, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Image, View } from 'react-native';
 import Container from '@app/components/container.js'
 import { FontAwesome5 } from "@expo/vector-icons";
-import Text, { Header, SubHeader } from '@app/components/text.js';
 import { useSelector, useDispatch } from 'react-redux';
 import { logoutUser } from "../../redux/actions/auth.js";
-import { current } from '@reduxjs/toolkit';
+import Text, { Header, SubHeader } from '@app/components/text.js';
 
-const ProfileScreen = (props) => {
+const ProfileScreen = props => {
     const isLoggedIn = useSelector(state => state.auth.loggedIn);
 
     const user_profile = useSelector(state => state.auth.currentUser);
@@ -22,112 +21,60 @@ const ProfileScreen = (props) => {
     return (
         <SafeAreaView>
             {isLoggedIn ? (
-                <Container>
-                    <Header style={styles.mainHeader}>
-                        Welcome to the Profile Screen!
-                    </Header>
-                    <Text style={styles.centeredText}>
-                        You are logged in as {user_profile.username}!
-                    </Text>
-                    <Image
-                        style={styles.profilePic}
-                        source={{uri: user_profile.profile_picture}}
-                    />
-                    <FontAwesome5.Button
-                        style={styles.googleButton}
-                        name="google"
-                        onPress={() => logoutHandler()}
-                    >
-                        <Text style={styles.googleText}>Log Out With Google</Text>
-                    </FontAwesome5.Button>
+                <Container style={styles.mainContainer}>
+                    <View style={styles.horizontalContainer}>
+                        <Image
+                            style={styles.profilePic}
+                            source={{ uri: user_profile.profile_picture }}
+                        />
+                        <View>
+                            <Text style={styles.userName}>
+                                {user_profile.username}
+                            </Text>
+                            <Text style={styles.fullName}>
+                                {user_profile.full_name}
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.twoHeadings}>
+                        <View style={styles.followers}>
+                            <Text style={styles.subheading}>
+                                Followers
+                            </Text>
+                            <Text>
+                                100
+                            </Text>
+                        </View>
+                        <View style={styles.following}>
+                            <Text style={styles.subheading}>
+                                Following
+                            </Text>
+                            <Text>
+                                55
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.twoHeadings}>
+                        <Text style={styles.subheading}>
+                            Exercise History
+                        </Text>
+                        <Text style={styles.subheading}>
+                            Saved Exercises
+                        </Text>
+                    </View>
+                    <View style={styles.googleButtonContainer}>
+                        <FontAwesome5.Button
+                            style={styles.googleButton}
+                            name="google"
+                            onPress={() => logoutHandler()}
+                        >
+                            <Text style={styles.googleText}>Log Out With Google</Text>
+                        </FontAwesome5.Button>
+                    </View>
                 </Container>
             ) : (
                     <Text>Logging Out.</Text>
                 )}
-        </SafeAreaView>
-    );
-};
-
-const styles = StyleSheet.create({
-    mainHeader: {
-        fontSize: 50,
-        marginTop: "55%",
-        textAlign: "center"
-    },
-    subHeader: {
-        fontSize: 18,
-        marginTop: 0,
-        textAlign: "center"
-    },
-    googleButton: {
-        height: 60,
-        paddingLeft: 50,
-        paddingRight: 50
-    },
-    centeredText: {
-        textAlign: 'center'
-    },
-    profilePic: {
-        alignSelf: 'center',
-        marginBottom: 15,
-        width: 60,
-        height: 60,
-        borderRadius: 5
-    },
-    googleText: {
-        color: 'white',
-        fontWeight: 'bold'
-    }
-});
-
-export default ProfileScreen;
-*/
-import React from 'react';
-import { Flex, SafeAreaView, StyleSheet, View } from 'react-native';
-import Container from '@app/components/container.js'
-import { FontAwesome5 } from "@expo/vector-icons";
-import Text, { Header, SubHeader } from '@app/components/text.js';
-
-const ProfileScreen = props => {
-    return (
-        <SafeAreaView>
-            <Container>
-                <Image
-                    style={styles.profilePic}
-                    source={{uri: user_profile.profile_picture}}
-                />
-                <Text style={styles.fullName}>
-                    Fullname
-                </Text>
-                <Text style={styles.userName}>
-                    Username
-                </Text>
-
-                <Text style ={styles.twoHeadings}>
-                    <Text style={styles.followers}>
-                        Followers
-                    </Text>
-                    <Text style={styles.following}>
-                        Following
-                    </Text>
-                </Text>
-                <Text style ={styles.twoHeadings}>
-                    <Text style={styles.exHist}>
-                        Exercise History
-                    </Text>
-                    <Text style={styles.exSaved}>
-                        Saved Exercises
-                    </Text>
-                </Text>
-
-                <FontAwesome5.Button
-                    style={styles.googleButton}
-                    name="google"
-                    onPress={() => props.setLoggedIn(false)}
-                >
-                    <Text style={styles.googleText}>Log Out With Google</Text>
-                </FontAwesome5.Button>
-            </Container>
         </SafeAreaView>
     )
 }
@@ -138,50 +85,56 @@ const styles = StyleSheet.create({
         marginTop: "55%",
         textAlign: "center"
     },
+    mainContainer: {
+        marginTop: 20,
+        paddingHorizontal: 35
+    },
     googleButton: {
         height: 60,
         paddingLeft: 50,
         paddingRight: 50
+    },
+    googleButtonContainer: {
+        marginTop: 25
     },
     googleText: {
         color: 'white',
         fontWeight: 'bold'
     },
     fullName: {
-        fontSize: 15,
-        fontWeight: 'bold',
-        marginTop: "30%",
-        paddingLeft: "55%"
+        fontSize: 15
     },
     userName: {
         fontSize: 15,
-        marginTop: "0%",
-        paddingLeft: "55%"
+        fontWeight: 'bold'
     },
     twoHeadings: {
-        paddingTop: "20%",
-        flex: 0.1,
-        flexDirection: "column"
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        marginTop: 20,
+        paddingHorizontal: 15
     },
     followers: {
         fontSize: 15,
-        paddingLeft: "22%"
+        fontWeight: 'bold'
     },
     following: {
         fontSize: 15,
-        paddingLeft: "5%"
-    },
-    exHist: {
-        fontSize: 15,
-        paddingLeft: "0%",
         fontWeight: 'bold'
-
     },
-    exSaved: {
-        fontSize: 15,
-        paddingLeft: "15%"
+    profilePic: {
+        width: 80,
+        height: 80,
+        borderRadius: 40
     },
-    
+    horizontalContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    subheading: {
+        fontWeight: 'bold'
+    }
 });
 
 export default ProfileScreen;
