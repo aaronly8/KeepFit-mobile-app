3c64e7feb5147224ea028c667d2c39c8b83c3c93
Max Friedman <max.friedman0@gmail.com>
2021-03-20 00:25:51 -0700

    Added search exercises and details functionality

--- KeepFit/src/screens/explore/exercises.js
+++ KeepFit/src/screens/explore/exercises.js
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, Button, Text, TouchableHighlight, View } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, Button, Text, Linking, View, TouchableHighlight } from 'react-native';
 import Container from '@app/components/container.js'
-import { Header } from '@app/components/text.js';
+import { Header, Subtext, Subheader } from '@app/components/text.js';
 import db from "../../firebase/firebase";
 import Exercise from "../../models/exercise"
+import ListItem from './listitem';
+import { MuscleGroupPicker, WorkoutCategoryPicker } from '../../components/pickers';
+
+const Tag = props => {
+    return (
+        <View style={styles.tag}>
+            <Text style={styles.tagText}>{props.value}</Text>
+        </View>
+    );
+};
 
 const DetailsScreen = props => {
     return (
         <SafeAreaView>
             <Button title="<< Back" onPress={() => props.detailsBackHandler()} />
-            <Header>
-                {props.exerciseName}
+            <Header style={styles.exerciseName}>
+                {props.exercise.name}
             </Header>
+            <Text style={styles.videoLink}
+             onPress={() => Linking.openURL(props.exercise.video_link)}>
+            Watch Exercise on Youtube
+            </Text>
+            <Text>
+                {props.exercise.description}
+            </Text>
+            <View style={styles.tagsContainer}>
+                <Tag value={props.exercise.category}>
+                </Tag>
+                {Object.entries(props.exercise.muscle_group).map(group =>
+                    <Tag value={group[1]}></Tag>
+                )}
+            </View>
         </SafeAreaView>
     );
 };
 
 const SearchExercisesScreen = props => {
+    const [exerciseDictionary, setExerciseDictionary] = useState({});
     const [filteredExerciseDictionary, setFilteredExerciseDictionary] = useState({});
     const [muscleGroupFilter1, setMuscleGroupFilter1] = useState("");
     const [muscleGroupFilter2, setMuscleGroupFilter2] = useState("");
@@ -24,15 +49,52 @@ const SearchExercisesScreen = props => {
     const [displayedDetails, setDisplayedDetails] = useState(null);
 
     useEffect(() => {
-        var exerciseDictionary = {};
+        var fetchedExerciseDictionary = {};
         db.collection(Exercise.collection_name).get().then(snapshot => {
             snapshot.forEach(doc => {
-                exerciseDictionary[doc.id] = doc.data();
+                fetchedExerciseDictionary[doc.id] = doc.data();
             });
-            setFilteredExerciseDictionary(exerciseDictionary);
-            console.log("just ran");
+            setFilteredExerciseDictionary(fetchedExerciseDictionary);
+            setExerciseDictionary(fetchedExerciseDictionary);
         });
     }, []);
+
+    useEffect(() => {
+        var newFilteredExerciseDictionary = {};
+
+        for(var exercise in exerciseDictionary) {
+            var exerciseDetails = exerciseDictionary[exercise];
+            var filterFlag1 = false, filterFlag2 = false, filterFlag3 = false;
+
+            if(muscleGroupFilter1 && muscleGroupFilter1 !== null) {
+                if(exerciseDetails.muscle_group.includes(muscleGroupFilter1)) {
+                    filterFlag1 = true;
+                } 
+            } else {
+                filterFlag1 = true;
+            }
+
+            if(muscleGroupFilter2 && muscleGroupFilter2 !== null) {
+                if(exerciseDetails.muscle_group.includes(muscleGroupFilter2)) {
+                    filterFlag2 = true;
+                } 
+            } else {
+                filterFlag2 = true;
+            }
+
+            if(categoryFilter && categoryFilter !== null) {
+                if(exerciseDetails.category === categoryFilter) {filterFlag3 = true;}
+                else {filterFlag3 = false;}
+            } else {
+                filterFlag3 = true;
+            }
+            console.log(filterFlag1, filterFlag2, filterFlag3);
+            if(filterFlag1 && filterFlag2 && filterFlag3) {
+                newFilteredExerciseDictionary[exercise] = exerciseDetails;
+            }
+        }
+        setFilteredExerciseDictionary(newFilteredExerciseDictionary);
+    }, [muscleGroupFilter1, muscleGroupFilter2, categoryFilter])
 
     const detailsBackHandler = () => {
         setDisplayedDetails(null);
@@ -42,22 +104,33 @@ const SearchExercisesScreen = props => {
         <SafeAreaView style={styles.searchContainer}>
                 {displayedDetails ? (
                     <DetailsScreen 
-                        exerciseName={filteredExerciseDictionary[displayedDetails].name}
+                        exerciseID = {displayedDetails[0]}
+                        exercise={filteredExerciseDictionary[displayedDetails[0]]}
                         detailsBackHandler={detailsBackHandler}
                     />
                 ) : (
                 <View style={styles.listView}>
                     <Button title="<< Back" onPress={() => props.changeScreenHandler("index")} />
-                    <ScrollView>
-                    {Object.entries(filteredExerciseDictionary).map(exercise =>
-                        <TouchableHighlight
-                        onPress={() => {
-                            setDisplayedDetails(exercise[0]);
-                        }}>
-                            <Text style={styles.exerciseText}>{exercise[1].name}</Text>
-                        </TouchableHighlight>
-                    )}
-                    </ScrollView>
+                    <View style={styles.filterContainer}>
+                        <MuscleGroupPicker onValueChange={value => {
+                            setMuscleGroupFilter1(value);
+                        }}></MuscleGroupPicker>
+                        <MuscleGroupPicker onValueChange={value => {
+                            setMuscleGroupFilter2(value);
+                        }}></MuscleGroupPicker>
+                        <WorkoutCategoryPicker onValueChange={value => {
+                            setCategoryFilter(value);
+                        }}></WorkoutCategoryPicker>
+                    </View>
+                    <View style={styles.scrollView}>
+                        <ScrollView style={{flexGrow: 1}}>
+                        {Object.entries(filteredExerciseDictionary).map(exercise =>
+                            <ListItem setDisplayedDetails={setDisplayedDetails} exercise={exercise}>
+
+                            </ListItem>
+                        )}
+                        </ScrollView>
+                    </View>
                 </View>
                 )}
         </SafeAreaView>
@@ -66,7 +139,8 @@ const SearchExercisesScreen = props => {
 
 const styles = StyleSheet.create({
     searchContainer: {
-        paddingHorizontal: 25
+        paddingHorizontal: 25,
+        flexGrow:1,
     },
     mainHeader: {
         fontSize: 50,
@@ -78,8 +152,52 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     listView: {
-        marginBottom: 200
-    }
+        marginBottom: 200,
+    },
+    filterContainer: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 3,
+        paddingBottom: 10,
+    },
+    scrollView: {
+        height: "72.5%"
+    },
+
+    // STYLES FOR DETAILS SCREEN START
+    exerciseName: {
+        textAlign: 'center',
+        paddingBottom: 20,
+    },
+    videoLink: {
+        textAlign: 'center',
+        color: 'blue',
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        paddingBottom: 20
+    },
+
+    tagsContainer: {
+        flexDirection: 'row',
+        paddingTop: 20,
+    },
+
+
+    //STYLES FOR TAG START
+    tag: {
+        width: 110,
+        height: 30,
+        backgroundColor: 'grey',
+        borderRadius: 10,
+        justifyContent: 'center',
+        shadowOffset:{  width: 0,  height: 2},
+        shadowColor: 'black',
+        shadowOpacity: 1.0,
+        marginRight: 10,
+      },
+
+      tagText: {
+        textAlign: 'center',
+      },
 });
 
 export default SearchExercisesScreen;
