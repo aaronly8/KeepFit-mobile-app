ad2c379bf1bbed34830d438e8b0f1130c59e9383
Max Friedman <max.friedman0@gmail.com>
2021-03-21 17:53:22 -0700

    small fix to displaying name/title in list

--- KeepFit/src/screens/explore/listitem.js
+++ KeepFit/src/screens/explore/listitem.js
@@ -9,7 +9,7 @@ const ListItem = props => {
             props.setDisplayedDetails(props.object);
         }}>
             <View style={styles.listItemContainer}>
-                <Text style={styles.objectText}>{props.object[1].name}</Text>
+                <Text style={styles.objectText}>{props.object[1].name}{props.object[1].title}</Text>
                 <Text style={styles.descriptionText} numberOfLines={2}>{props.object[1].description}</Text>
             </View>
         </TouchableHighlight>
